79463ed1447ee8f30bf399d6ec064958cac1c585
Will Sabol <wsabol39@gmail.com>
2019-05-27 21:00:48 -0500

    typo

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import 'bulma/css/bulma.css';
 import App from './App'
+import * as serviceWorker from './serviceWorker';
 
 render(<App />, document.getElementById('root'));
 
