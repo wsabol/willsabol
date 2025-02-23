59174df23e75713a85b302efa061e61d30a2e3e5
Will Sabol <wsabol39@gmail.com>
2025-02-23 09:47:26 -0600

    remove

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import 'bulma/css/bulma.css';
-import App from './App'
-import * as serviceWorker from './serviceWorker';
-
-render(<App />, document.getElementById('root'));
-
-serviceWorker.register();
