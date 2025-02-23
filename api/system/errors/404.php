e99394911fb8f0a0be1d553b3bdc3842888ca470
Will Sabol <wsabol39@gmail.com>
2025-02-23 10:05:59 -0600

    api

---
+++ api/system/errors/404.php
@@ -0,0 +1,38 @@
+<?php
+
+use Portfolio\CommonEndpoint;
+
+require '../../../includes/autoload.php';
+
+class System_Error_404_Endpoint extends CommonEndpoint {
+
+    public function __construct() {
+        // $this->restrict_access();
+
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        switch ($method) {
+            case 'GET':
+                $this->get_response();
+                break;
+
+            default:
+                $this->method_not_allowed();
+        }
+    }
+
+    public function get_response() {
+        $data = (object)$_GET;
+        $code = 404;
+        $message = 'The requested URI is not found';
+
+        if (isset($data->request_uri)) {
+            $message .= ": {$data->request_uri}";
+        }
+
+        echo $this->response($data, $code, $message);
+        return true;
+    }
+}
+
+new System_Error_404_Endpoint();
